fc4f41a9c095f7663db465d6696f101bf9085986
OFRIN <josanghyeokn@gmail.com>
2020-09-08 00:12:48 +0900

    update README.md

--- create_xlsx.py
+++ create_xlsx.py
@@ -1,6 +1,7 @@
 # Copyright (C) 2020 * Ltd. All rights reserved.
 # author : Sanghyeon Jo <josanghyeokn@gmail.com>
 
+import os
 import glob
 
 from crawling import get_soup
@@ -10,27 +11,27 @@ from utils import download_file_from_url, read_json
 # 1. Load json files
 json_paths = sorted(glob.glob('./data/*.json'))
 for json_index, json_path in enumerate(json_paths):
-    print('{:02d}. {}'.format(json_index + 1, json_path))
+    print('{:02d}. {}'.format(json_index + 1, os.path.basename(json_path)))
 
-json_index = int(input('Select index => '))
+json_index = int(input('Select index => ')) - 1
 data = read_json(json_paths[json_index])
 
 excel_path = './results/{}.xlsx'.format(data['name'])
 links = data['links']
 
 # 2. Crawling and merge dataset
-writer = XLSX_Writer(excel_path, ['Name', 'URL'])
+writer = XLSX_Writer(excel_path, ['Index', 'Name', 'URL'])
 
 for link in links:
     soup = get_soup(link)
     papers = soup.select('dt > a')
-
-    main_url = 'https://openaccess.thecvf.com/'
+    
     for paper in papers:
-        pdf_url = main_url + paper.get('href')
+        pdf_url = 'https://openaccess.thecvf.com/' + paper.get('href')
         pdf_url = pdf_url.replace('.html', '.pdf')
         pdf_url = pdf_url.replace('/html/', '/papers/')
 
-        writer([paper.text, pdf_url])
+        writer([writer.row_index - 1, paper.text, pdf_url])
 
 writer.close()
+
--- excel.py
+++ excel.py
@@ -8,6 +8,8 @@ class XLSX_Writer:
     def __init__(self, excel_path : str, class_names : list):
         if os.path.isfile(excel_path):
            raise ValueError 
+
+        self.excel_path = excel_path
         
         self.book = openpyxl.Workbook()
         self.book.remove(self.book.get_sheet_by_name('Sheet'))
@@ -18,14 +20,29 @@ class XLSX_Writer:
 
         self.update(class_names)
 
-    def __call__(self, dataset):
+    def __call__(self, dataset : list):
         self.update(dataset)
 
-    def update(self, dataset):
+    def update(self, dataset : list):
         for index, data in enumerate(dataset):
             column = chr(self.start_ascii + index)
             self.sheet['{}{}'.format(column, self.row_index)] = data
         self.row_index += 1
 
-    def close(self, dataset):
+    def close(self):
         self.book.save(self.excel_path)
+
+class XLSX_Reader:
+    def __init__(self, excel_path : str, class_names : list):
+        if not os.path.isfile(excel_path):
+           raise ValueError 
+
+        self.book = openpyxl.load_workbook(excel_path)
+        self.sheet = book.get_sheet_by_name('Papers')
+
+        self.get_fn = lambda name: sheet[name].value
+
+        self.class_names = class_names
+
+        self.start_ascii = 65
+        self.row_index = 2
