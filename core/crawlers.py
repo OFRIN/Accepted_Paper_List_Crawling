a6dafd96666f7a9efd0c213615fb7057d7634a6c
OFRIN <josanghyeokn@gmail.com>
2021-05-31 13:50:44 +0900

    add crawling_utils

---
+++ core/crawlers.py
@@ -0,0 +1,21 @@
+
+
+def get_soup(url, option='requests', delay=1):
+    if option == 'requests':
+        req = requests.get(url)
+        page_source = req.text
+    else:
+        options = webdriver.ChromeOptions()
+        options.add_argument('--ignore-certificate-errors')
+        options.add_argument('--incognito')
+        options.add_argument('--headless')
+
+        driver = webdriver.Chrome("./data/chromedriver", chrome_options=options)
+        driver.get(url)
+
+        time.sleep(delay)
+
+        page_source = driver.page_source
+
+    soup = BeautifulSoup(page_source, 'html.parser')
+    return soup
--- examples/ex_arxiv.py
+++ examples/ex_arxiv.py
@@ -1,7 +1,10 @@
 import arxiv
 
 # https://pypi.org/project/arxiv/
-search = arxiv.Search("Inferring Super-Resolution Depth from a Moving Light-Source Enhanced RGB-D Sensor: A Variational Approach", max_results=1)
+# search = arxiv.Search("Inferring Super-Resolution Depth from a Moving Light-Source Enhanced RGB-D Sensor: A Variational Approach", max_results=1)
+search = arxiv.Search("Puzzle-CAM: Improved localization via matching partial and full features", max_results=5)
+
+# ERROR
 
 for search in search.get():
     print(search.title)
